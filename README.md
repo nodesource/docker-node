dd13ec7362059b4f56c559775d8d97670cd4619d
William Blankenship <william.jblankenship@gmail.com>
2015-03-04 15:54:40 -0600

    Let there be iojs

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Dockerfiles for building docker images with the [NodeSource Node.js](https://git
 
 # Structure
 
-* `latest` on all images currently points to the latest 0.10 release
-* `0.10` on all images currently points to the latest 0.10 release
-* `0.12` on all images currently points to the latest 0.12 release
+* `latest` on all images currently points to the latest 0.10 release of node
+* `0.10` on all images currently points to the latest 0.10 release of node
+* `0.12` on all images currently points to the latest 0.12 release of node
+* `1` on all images currently points to the latest 1.X release of iojs
+* `node` on all images currently points to the latest 0.12 release of node
+* `iojs` on all images currently points to the latest 1.X release of iojs
+* All images alias `node-[version]` and `iojs-[version]` to `[version]` for tags
 
 ## Debian-based images
 
@@ -28,6 +32,7 @@ Dockerfiles for building docker images with the [NodeSource Node.js](https://git
   * Node 0.10.35 - `docker pull nodesource/jessie:0.10.35`
   * Node 0.10.36 - `docker pull nodesource/jessie:0.10.36`
   * Node 0.12.0  - `docker pull nodesource/jessie:0.12.0`
+  * iojs 1.4.3   - `docker pull nodesource/jessie:1.4.3`
 * [**Debian unstable**](https://registry.hub.docker.com/u/nodesource/sid/) (sid) - `docker pull nodesource/sid`
   * Node 0.10.30 - `docker pull nodesource/sid:0.10.30`
   * Node 0.10.31 - `docker pull nodesource/sid:0.10.31`
@@ -37,6 +42,7 @@ Dockerfiles for building docker images with the [NodeSource Node.js](https://git
   * Node 0.10.35 - `docker pull nodesource/sid:0.10.35`
   * Node 0.10.36 - `docker pull nodesource/sid:0.10.36`
   * Node 0.12.0  - `docker pull nodesource/sid:0.12.0`
+  * iojs 1.4.3   - `docker pull nodesource/sid:1.4.3`
 
 ## Ubuntu-based images
 
@@ -58,6 +64,7 @@ Dockerfiles for building docker images with the [NodeSource Node.js](https://git
   * Node 0.10.35 - `docker pull nodesource/trusty:0.10.35`
   * Node 0.10.36 - `docker pull nodesource/trusty:0.10.36`
   * Node 0.12.0  - `docker pull nodesource/trusty:0.12.0`
+  * iojs 1.4.3   - `docker pull nodesource/trusty:1.4.3`
 * [**Ubuntu 14.10**](https://registry.hub.docker.com/u/nodesource/utopic/)  (Utopic Unicorn) - `docker pull nodesource/utopic`
   * Node 0.10.32 - `docker pull nodesource/utopic:0.10.32`
   * Node 0.10.33 - `docker pull nodesource/utopic:0.10.33`
--- debian.js
+++ debian.js
@@ -34,6 +34,7 @@ var commonPkgs = [  'apt-transport-https',
 // Debian
 dists['debian']['wheezy'] = []
 dists['debian']['wheezy']['node'] = []
+dists['debian']['wheezy']['iojs'] = []
 dists['debian']['wheezy']['node']['0.10.30']  = {}
 dists['debian']['wheezy']['node']['0.10.31']  = {deb:'nodejs_0.10.31-1chl1~wheezy1_amd64.deb'}
 dists['debian']['wheezy']['node']['0.10.32']  = {}
@@ -45,6 +46,7 @@ dists['debian']['wheezy']['node']['0.12.0']   = {url: 'https://deb.nodesource.co
 
 dists['debian']['jessie'] = []
 dists['debian']['jessie']['node'] = []
+dists['debian']['jessie']['iojs'] = []
 dists['debian']['jessie']['node']['0.10.30']  = {}
 dists['debian']['jessie']['node']['0.10.31']  = {deb:'nodejs_0.10.31-1chl1~jessie1_amd64.deb'}
 dists['debian']['jessie']['node']['0.10.32']  = {}
@@ -53,9 +55,11 @@ dists['debian']['jessie']['node']['0.10.34']  = {}
 dists['debian']['jessie']['node']['0.10.35']  = {}
 dists['debian']['jessie']['node']['0.10.36']  = {}
 dists['debian']['jessie']['node']['0.12.0']   = {url: 'https://deb.nodesource.com/node012/pool/main/n/nodejs/'}
+dists['debian']['jessie']['iojs']['1.4.3']    = {url:'https://deb.nodesource.com/iojs_1.x/pool/main/i/iojs/',deb:'iojs_1.4.3-1nodesource1~jessie1_amd64.deb'}
 
 dists['debian']['sid'] = []
 dists['debian']['sid']['node'] = []
+dists['debian']['sid']['iojs'] = []
 dists['debian']['sid']['node']['0.10.30']     = {}
 dists['debian']['sid']['node']['0.10.31']     = {deb:'nodejs_0.10.31-1chl1~sid1_amd64.deb'}
 dists['debian']['sid']['node']['0.10.32']     = {}
@@ -64,15 +68,12 @@ dists['debian']['sid']['node']['0.10.34']     = {}
 dists['debian']['sid']['node']['0.10.35']     = {}
 dists['debian']['sid']['node']['0.10.36']     = {}
 dists['debian']['sid']['node']['0.12.0']      = {url: 'https://deb.nodesource.com/node012/pool/main/n/nodejs/'}
-
-// Debian aliases
-dists['debian']['wheezy']
-dists['debian']['jessie']
-dists['debian']['sid']
+dists['debian']['sid']['iojs']['1.4.3']       = {url:'https://deb.nodesource.com/iojs_1.x/pool/main/i/iojs/',deb:'iojs_1.4.3-1nodesource1~sid1_amd64.deb'}
 
 // Ubuntu
 dists['ubuntu']['precise']  = []
 dists['ubuntu']['precise']['node']  = []
+dists['ubuntu']['precise']['iojs']  = []
 dists['ubuntu']['precise']['node']['0.10.30'] = {}
 dists['ubuntu']['precise']['node']['0.10.31'] = {deb:'nodejs_0.10.31-1chl1~precise1_amd64.deb'}
 dists['ubuntu']['precise']['node']['0.10.32'] = {}
@@ -92,6 +93,8 @@ dists['ubuntu']['trusty']['node']['0.10.34']  = {}
 dists['ubuntu']['trusty']['node']['0.10.35']  = {}
 dists['ubuntu']['trusty']['node']['0.10.36']  = {}
 dists['ubuntu']['trusty']['node']['0.12.0']   = {url: 'https://deb.nodesource.com/node012/pool/main/n/nodejs/'}
+dists['ubuntu']['trusty']['iojs']   = []
+dists['ubuntu']['trusty']['iojs']['1.4.3']    = {url:'https://deb.nodesource.com/iojs_1.x/pool/main/i/iojs/',deb:'iojs_1.4.3-1nodesource1~trusty1_amd64.deb'}
 
 dists['ubuntu']['utopic']   = []
 dists['ubuntu']['utopic']['node']   = []
@@ -101,6 +104,9 @@ dists['ubuntu']['utopic']['node']['0.10.34']  = {}
 dists['ubuntu']['utopic']['node']['0.10.35']  = {}
 dists['ubuntu']['utopic']['node']['0.10.36']  = {}
 dists['ubuntu']['utopic']['node']['0.12.0']   = {url: 'https://deb.nodesource.com/node012/pool/main/n/nodejs/'}
+dists['ubuntu']['utopic']['iojs']   = []
+dists['ubuntu']['utopic']['iojs']['1.4.3']    = {url:'https://deb.nodesource.com/iojs_1.x/pool/main/i/iojs/',deb:'iojs_1.4.3-1nodesource1~utopic1_amd64.deb'}
+
 
 /**
  * Define string constants
